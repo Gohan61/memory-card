0cc8e09f700611bc608a3b9d9cb05dd5f21ad9f7
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-04-29 16:21:12 +0200

    Feat: get images based on search term

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,15 @@ function App() {
   return (
     <>
       <Scoreboard></Scoreboard>
-      <GetImages></GetImages>
+      {/* <GetImages search={"Michael Scott"}></GetImages>
+      <GetImages search={"Toby Flenderson"}></GetImages>
+      <GetImages search={"Dwight Schrute"}></GetImages>
+      <GetImages search={'Phyllis Vance'}></GetImages>
+      <GetImages search={'Ryan Howard'}></GetImages>
+      <GetImages search={'Creed Bratton'}></GetImages>
+      <GetImages search={'Stanley Hudson'}></GetImages>
+      <GetImages search={'Angela Martin'}></GetImages>
+      <GetImages search={'Oscar Martinez'}></GetImages> */}
     </>
   );
 }
--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -2,19 +2,21 @@ import { useEffect, useState } from "react";
 
 const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 
-export default function GetImages() {
+export default function GetImages(props) {
   const [image, setImage] = useState([]);
+  console.log(props);
 
   useEffect(() => {
     const getImage = () => {
       fetch(
-        `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}=random`,
+        `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}=${props.search}`,
         {
           mode: "cors",
         },
       )
         .then(function (response) {
           console.log(response);
+
           return response.json();
         })
         .then(function (response) {
